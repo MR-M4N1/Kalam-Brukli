5040c75a213de010a60e2859fab6354acb8c10c8
CALL ME M <134063565+MR-M4N1@users.noreply.github.com>
2024-07-20 11:11:03 -0700

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,30 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entery) => {
+    console.log(entery);
+    if (entery.isIntersecting) {
+      entery.target.classList.add('show');
+    }
+    else {
+      entery.target.classList.remove('show');
+    }
+  });
+});
+
+console.log(document.styleSheets[0].cssRules[87]);
+
+const hiddenElemnts = document.querySelectorAll('.hidden');
+hiddenElemnts.forEach((el) => observer.observe(el));
+
+window.addEventListener("load", function () {
+  var HeroPaddTransition = document.getElementById("Hero_Section");
+  HeroPaddTransition.style.padding = "0 10%";
+  HeroPaddTransition.style.transition = "padding 1s";
+  var BtnTranisition = document.getElementById("Body_BTN");
+  BtnTranisition.style.margin = " 0 0 0 30px";
+  BtnTranisition.style.transition = "margin-left 1s";
+
+})
+
 document.addEventListener("DOMContentLoaded", function () {
   var IconImg1 = document.getElementById("First_Icon");
   if (IconImg1) {
@@ -97,58 +124,58 @@ function ShowOff4() {
 
 
 document.getElementById("First_More").addEventListener("mouseenter", function () {
-    document.getElementById("First_Moreinfo").style.bottom = "-1435px";
-    document.getElementById("First_Moreinfo").style.display = "flex";
-    let st = document.styleSheets[0].cssRules[84]; 
-    st.style.display = "inline";
-  });
+  document.getElementById("First_Moreinfo").style.bottom = "-110px";
+  document.getElementById("First_Moreinfo").style.display = "flex";
+  let st = document.styleSheets[0].cssRules[87];
+  st.style.display = "inline";
+});
 
-  document.getElementById("First_More").addEventListener("mouseleave", function () {
-    document.getElementById("First_Moreinfo").style.bottom = "-1400px";
-    document.getElementById("First_Moreinfo").style.display = "none";
-    let st = document.styleSheets[0].cssRules[84]; 
-    st.style.display = "none";
-  });
+document.getElementById("First_More").addEventListener("mouseleave", function () {
+  document.getElementById("First_Moreinfo").style.bottom = "-80px";
+  document.getElementById("First_Moreinfo").style.display = "none";
+  let st = document.styleSheets[0].cssRules[87];
+  st.style.display = "none";
+});
 
-  document.getElementById("Secend_More").addEventListener("mouseenter", function () {
-    document.getElementById("Secend_MoreInfo").style.bottom = "-1435px";
-    document.getElementById("Secend_MoreInfo").style.display = "flex";
-    let st = document.styleSheets[0].cssRules[86]; 
-    st.style.display = "inline";
-  });
+document.getElementById("Secend_More").addEventListener("mouseenter", function () {
+  document.getElementById("Secend_MoreInfo").style.bottom = "-110px";
+  document.getElementById("Secend_MoreInfo").style.display = "flex";
+  let st = document.styleSheets[0].cssRules[89];
+  st.style.display = "inline";
+});
 
-  document.getElementById("Secend_More").addEventListener("mouseleave", function () {
-    document.getElementById("Secend_MoreInfo").style.bottom = "-1400px";
-    document.getElementById("Secend_MoreInfo").style.display = "none";
-    let st = document.styleSheets[0].cssRules[86]; 
-    st.style.display = "none";
-  });
+document.getElementById("Secend_More").addEventListener("mouseleave", function () {
+  document.getElementById("Secend_MoreInfo").style.bottom = "-1400px";
+  document.getElementById("Secend_MoreInfo").style.display = "none";
+  let st = document.styleSheets[0].cssRules[89];
+  st.style.display = "none";
+});
 
-  document.getElementById("Thirth_More").addEventListener("mouseenter", function () {
-    document.getElementById("Thirth_MorInfo").style.bottom = "-1435px";
-    document.getElementById("Thirth_MorInfo").style.display = "flex";
-    let st2 = document.styleSheets[0].cssRules[88]; 
-    st2.style.display = "inline";
-  });
+document.getElementById("Thirth_More").addEventListener("mouseenter", function () {
+  document.getElementById("Thirth_MorInfo").style.bottom = "-110px";
+  document.getElementById("Thirth_MorInfo").style.display = "flex";
+  let st2 = document.styleSheets[0].cssRules[91];
+  st2.style.display = "inline";
+});
 
-  document.getElementById("Thirth_More").addEventListener("mouseleave", function () {
-    document.getElementById("Thirth_MorInfo").style.bottom = "-1400px";
-    document.getElementById("Thirth_MorInfo").style.display = "none";
-    let st2 = document.styleSheets[0].cssRules[88]; 
-    st2.style.display = "none";
-  });
+document.getElementById("Thirth_More").addEventListener("mouseleave", function () {
+  document.getElementById("Thirth_MorInfo").style.bottom = "-1400px";
+  document.getElementById("Thirth_MorInfo").style.display = "none";
+  let st2 = document.styleSheets[0].cssRules[91];
+  st2.style.display = "none";
+});
 
-  document.getElementById("Forth_More").addEventListener("mouseenter", function () {
-    document.getElementById("Forth_MorInfo").style.bottom = "-1435px";
-    document.getElementById("Forth_MorInfo").style.display = "flex";
-    let st2 = document.styleSheets[0].cssRules[90]; 
-    st2.style.display = "inline";
-  });
+document.getElementById("Forth_More").addEventListener("mouseenter", function () {
+  document.getElementById("Forth_MorInfo").style.bottom = "-110px";
+  document.getElementById("Forth_MorInfo").style.display = "flex";
+  let st2 = document.styleSheets[0].cssRules[93];
+  st2.style.display = "inline";
+});
 
-  document.getElementById("Forth_More").addEventListener("mouseleave", function () {
-    document.getElementById("Forth_MorInfo").style.bottom = "-1400px";
-    document.getElementById("Forth_MorInfo").style.display = "none";
-    let st2 = document.styleSheets[0].cssRules[90]; 
-    st2.style.display = "none";
-  });
+document.getElementById("Forth_More").addEventListener("mouseleave", function () {
+  document.getElementById("Forth_MorInfo").style.bottom = "0px";
+  document.getElementById("Forth_MorInfo").style.display = "none";
+  let st2 = document.styleSheets[0].cssRules[93];
+  st2.style.display = "none";
+});
 
